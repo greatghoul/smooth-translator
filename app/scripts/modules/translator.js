85b5644de34a4292ec91d42047e31120a57f2172
greatghoul <greatghoul@gmail.com>
2022-02-19 22:40:47 +0800

    remove gulp tasks and check if text is a word

--- app/scripts/modules/translator.js
+++ app/scripts/modules/translator.js
@@ -7,18 +7,36 @@ const buildURL = text => {
   return `https://dict.youdao.com/w/eng/${encodeURIComponent(text)}/#keyfrom=dict2.index`
 }
 
+const isWord = text => {
+  return /^[a-z]+$/img.test(text)
+}
+
+const translateWord = text => {
+  const url = buildURL(text)
+  return fetch(url, {
+    credentials: "include",
+    cache: "force-cache",
+    redirect: "follow",
+    referrer: url,
+  }).then(resp => {
+    return resp.text()
+  }).then(text => {
+    console.log(text)
+  })
+}
+
+const translatePhase = text => {
+
+}
+
 export function translate (text) {
   return new Promise((resolve, reject) => {
     if (!text) {
       reject(RESULT_FAILURE)
+    } else if (isWord(text)) {
+      translateWord(text).then(resolve).catch(reject)
     } else {
-      const url = buildURL(text)
-      return fetch(url, {
-        credentials: "include",
-        cache: "force-cache",
-        redirect: "follow",
-        referrer: url,
-      }).then(resp => resp.text()).then(text => console.log(text)).then(() => resolve(RESULT_FAILURE))
+      translatePhase(text).then(resolve).catch(reject)
     }
   })
 }
